ace472da2726d2ef3435a9f33f706058caa77807
vitor.argeri@aluno.senai.br <vitor.argeri@aluno.senai.br>
2025-09-30 09:32:29 -0300

    feat: add footer component

--- src/Components/Footer/page.jsx
+++ src/Components/Footer/page.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import styles from './footer.module.css'
 
 export default function page() {
     return (
         <div className={styles.container}>
-
+            <p className={styles.logo}>🍺 Project API Beer</p>
+            <p className={styles.text}>Desenvolvido por <a href="https://github.com/VitorArgeri" target="_blank" rel="noopener noreferrer" className={styles.name}>Vitor Argeri</a></p>
         </div>
     )
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../Components/Header/page'
+import Footer from '../Components/Footer/page'
 
 export default function page() {
     return (
         <div>
             <Header />
+            <Footer />
         </div>
     )
 }
